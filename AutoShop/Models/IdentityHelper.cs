cb64e2f9846a6902524f19117638174f43f6c85e
dom summers <summers8734@student.cptc.edu>
2021-04-21 12:50:53 -0700

    fixed using

--- AutoShop/Models/IdentityHelper.cs
+++ AutoShop/Models/IdentityHelper.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
 namespace AutoShop.Models
 {
     public class IdentityHelper
