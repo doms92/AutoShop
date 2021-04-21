4d1ee851f0cbe448a603f8fe621d7a706c0a022c
dom summers <summers8734@student.cptc.edu>
2021-04-21 12:46:33 -0700

    Added Abstract using to Identity Helper

--- AutoShop/Models/IdentityHelper.cs
+++ AutoShop/Models/IdentityHelper.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using Microsoft.Extensions.DependencyInjection.Abstractions;
 namespace AutoShop.Models
 {
     public class IdentityHelper
