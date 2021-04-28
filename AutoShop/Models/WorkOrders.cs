cef9edf6e98af84dbca84f4c652bb6a29a2b4568
dom summers <summers8734@student.cptc.edu>
2021-04-28 16:19:32 -0700

    Create Work Order Model

---
+++ AutoShop/Models/WorkOrders.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoShop.Models
+{
+    public abstract class WorkOrders
+    {
+        [Key]
+        public int WorkOrderId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        
+    }
+}
