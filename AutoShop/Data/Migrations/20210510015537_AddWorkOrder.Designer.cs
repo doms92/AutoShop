4dce6459bd274787ae6b39c1192a0d1762b381b4
doms92 <72600993+doms92@users.noreply.github.com>
2021-05-20 17:15:44 -0700

    Issue#4 shop manager create/delete work orders (#16)
    * Create Work Order Model
    
    * Added WorkOrder DB Set
    
    * Add WorkOrder Dbset
    
    * Add Migration for WorkOrders
    
    * Create/Delete/Edit Work Orders

--- AutoShop/Data/Migrations/20210510015537_AddWorkOrder.Designer.cs
+++ AutoShop/Data/Migrations/20210510015537_AddWorkOrder.Designer.cs
@@ -4,14 +4,16 @@ using AutoShop.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AutoShop.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210510015537_AddWorkOrder")]
+    partial class AddWorkOrder
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
