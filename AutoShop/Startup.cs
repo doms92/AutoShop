ca6557e588a4f778f9686a14bcb789eccb89f435
doms92 <domos92@gmail.com>
2021-06-14 16:07:25 -0700

    Added Connection String

--- AutoShop/Startup.cs
+++ AutoShop/Startup.cs
@@ -25,9 +25,15 @@ namespace AutoShop
 
         public IConfiguration Configuration { get; }
 
+      
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString
+                ("DefaultConnection")));
+                    )
             services.AddScoped<IWorkOrderRepository, WorkOrderRepository>();
             services.AddScoped<IAutoPartRepository, AutoPartsRepository>();
             services.AddDbContext<ApplicationDbContext>(options =>
